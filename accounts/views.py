54e71ab142d21a8c5d76f2d3602c9bb9af0d3c8a
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-10-20 12:49:04 -0600

    Creacion de Registro Usuario

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login, logout, authenticate
+from .models import Profile
 
 # Create your views here.
+def register(request):
+    User = get_user_model()
+
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        first_name = request.POST.get('first_name','')
+        last_name = request.POST.get('last_name','')
+        password = request.POST.get('password1', '')
+        
+        user = User.objects.create_user(username = username,
+                                        password = password,
+                                        first_name = first_name,
+                                        last_name = last_name,)
+        profile = Profile.objects.create(user = user)
+        profile.save()
+
+        user = authenticate(username=username,password=password)    
+        if user is not None:
+            login(request,profile)
+        return redirect("home")
+    
+    return render(request,"register.html")
--- chifleta/urls.py
+++ chifleta/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register, name="register")
+    
 ]
 
 # Servir archivos de media en modo depuración
