90ca34d04e702b6843881392b7c2287a628578ce
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-10-28 11:14:11 -0600

    Terminado Views de Account

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model,login, alogin, logout, authenticate
+from django.contrib.auth import get_user_model,login, logout, authenticate
 from django.contrib.auth.models import User
 from .models import Profile
 import asyncio
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+
+def is_valid_name(str):
+    return len(str) < 3 or len(str) > 50
+
 
 # Create your views here.
 def register(request):
@@ -23,12 +29,12 @@ def register(request):
                 "error":"Escriba un minimo de 6 caracteres para el usuario."
             }
             return render(request,"register.html",context)
-        if len(first_name) < 3 or len(first_name) > 50:
+        if not is_valid_name(first_name):
             context={
                 "error":"Escriba un nombre en un rango de 3 a 50 caracteres."
             }
             return render(request,"register.html",context)
-        if len(last_name) < 3 or len(last_name) > 50:
+        if not is_valid_name(last_name):
             context={
                 "error":"Escriba un apellido en un rango de 3 a 50 caracteres."
             }
@@ -85,7 +91,6 @@ def log_in(request):
     if request.method == "POST":
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")
-        #TODO: VERIFICAR SI SE LOGEA CORRECTAMENTE
         user = authenticate(username=username, password=password)
         if user is None:
             context={
@@ -95,3 +100,54 @@ def log_in(request):
         login(request,user)
         return redirect("user",user.username)
     return render(request,"login.html" )
+
+
+@login_required(redirect_field_name="login")
+@require_http_methods(["POST"])
+def log_out(request):
+    
+    logout(request)
+    
+    return redirect("login")
+
+@login_required(redirect_field_name="login")
+@require_http_methods(["POST"])
+def delete_user(request):
+    request.user.delete()
+    return redirect("login")
+
+@login_required(redirect_field_name="user")
+@require_http_methods(["POST"])
+def update_user(request):
+    user = request.user
+    profile =  Profile.objects.get(user = user)
+    first_name = request.POST.get("first_name","")
+    last_name = request.POST.get("last_name","")
+    bio = request.POST.get("bio","")
+    context={
+        "username": user.username,
+        "first_name": user.first_name,
+        "last_name" : user.last_name,
+        "bio" : profile.bio,
+        "avatar" : profile.avatar,
+        "followers_count" : profile.followers_count,
+        "following_count" : profile.following_count
+    }
+    if not is_valid_name(first_name):
+        context["update_error"] = "Escriba un nombre en un rango de 3 a 50 caracteres."
+        return render(request,"user.html", context)
+    if not is_valid_name(last_name):
+        context["update_error"] = "Escriba un apellido en un rango de 3 a 50 caracteres."
+        return render(request,"user.html", context)
+    if len(bio) > 160:
+        context["update_error"] = "Escriba una bio menor de 160 caracteres."
+        return render(request,"user.html", context)
+    
+    user.first_name = first_name
+    user.last_name = last_name
+    profile.bio = bio
+    
+    user.save()
+    profile.save()
+    
+    return redirect("user")
--- chifleta/urls.py
+++ chifleta/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import register, user, log_in
+from accounts.views import register, user, log_in, log_out, delete_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register, name="register"),
     path('user/<str:username>/', user, name="user"),
-    path('login/', log_in, name="login")
+    path('login/', log_in, name="login"),
+    path('logout/', log_out, name="logout"),
+    path('delete_user/', delete_user, name="delete_user")
     
 ]
 
