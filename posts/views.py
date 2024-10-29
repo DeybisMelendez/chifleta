e2b53439311b636b9d2f69a633b319e34d15db9e
NelsonCordoba <nelsoncordero1011@gmail.com>
2024-10-29 10:20:21 -0600

    Avance para post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.models import Profile, Follow
+from .models import Post
 
 # Create your views here.
+@login_required(redirect_field_name="login")
+def feed(request):
+    
+    user = request.user
+    profile = Profile.objects.get(user = user)
+    followeds = Follow.objects.filter(follower = profile)
+    
+    for followed in followeds:
+        posts = Post.objects.filter(profile = followed)
+    
+    return
